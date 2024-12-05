b0a3c6b5e959bc669cb9ecc20e1562ecd8d37a8c
CyberViola <62507629+CyberViola@users.noreply.github.com>
2024-12-05 23:30:27 +0100

    Update pagerank.c

--- pagerank.c
+++ pagerank.c
@@ -229,9 +229,9 @@ void *consumatori(void *arg) {
             erroreInput("Errore: Indice del buffer non valido.\n");
         }
         // prende i nodi dal buffer
-        nu = a->buffer[(*(a->pcindex)*2) % (Buf_size)];
-        ne = a->buffer[(*(a->pcindex)*2 + 1) % (Buf_size)];
-        *(a->pcindex) = (*(a->pcindex)+1) % Buf_size;
+        nu = a->buffer[(*(a->pcindex))%(Buf_size)];
+        ne = a->buffer[(*(a->pcindex) + 1)%(Buf_size)];
+        *(a->pcindex) = (*(a->pcindex)+1)%Buf_size;
         pthread_mutex_unlock(a->mutex);
         sem_post(a->sem_free_slots);
 
