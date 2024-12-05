872327b275b8768bafdb796632dd3e1d9fc45635
CyberViola <62507629+CyberViola@users.noreply.github.com>
2024-12-05 22:08:13 +0100

    Update pagerank.c

--- pagerank.c
+++ pagerank.c
@@ -224,15 +224,15 @@ void *consumatori(void *arg) {
     while (true) {
         sem_wait(a->sem_data_items);
         pthread_mutex_lock(a->mutex);
-        if (*(a->pcindex)<0 || *(a->pcindex) >= Buf_size) {
+        if (*(a->pcindex)<0 || *(a->pcindex) >= Buf_size*2) {
             pthread_mutex_unlock(a->mutex);
             erroreInput("Errore: Indice del buffer non valido.\n");
         }
         // prende i nodi dal buffer
-    nu = a->buffer[*(a->pcindex)%(Buf_size)];
-    *(a->pcindex) = (*(a->pcindex) +1)%(Buf_size);
-    ne = a->buffer[*(a->pcindex)%(Buf_size)];
-    *(a->pcindex) = (*(a->pcindex)+1)%(Buf_size);
+    nu = a->buffer[*(a->pcindex)%(Buf_size*2)];
+    *(a->pcindex) = (*(a->pcindex) +1)%(Buf_size*2);
+    ne = a->buffer[*(a->pcindex)%(Buf_size*2)];
+    *(a->pcindex) = (*(a->pcindex)+1)%(Buf_size*2);
         pthread_mutex_unlock(a->mutex);
         sem_post(a->sem_free_slots);
 
